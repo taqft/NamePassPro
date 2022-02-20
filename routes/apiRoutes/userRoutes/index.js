6a0d6363536cddb87c2400e04c02325894e76628
Bemourn <70115233+koenib@users.noreply.github.com>
2022-02-20 13:21:12 -0700

    hash and verification on signup/signin

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 const {
   createUser,
-  getAllUsers,
+  // getAllUsers,
   getUserById,
   login,
   signupHandler,
@@ -10,7 +10,7 @@ const {
 } = require('../../../controllers/userController');
 
 router.route('/')
-  .get(getAllUsers) // remove after testing
+  // .get(getAllUsers) // remove after testing
   .post(createUser);
 
 router.post('/signup', signupHandler);
