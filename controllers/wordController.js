dbcd374c5647e568bf84dd972919160f1ce01b79
Bemourn <70115233+koenib@users.noreply.github.com>
2022-02-19 20:01:39 -0700

    word api routes; faster pass generation

--- controllers/wordController.js
+++ controllers/wordController.js
@@ -2,27 +2,39 @@
 const {
   Word
 } = require('../models');
+const {
+  Op
+} = require("sequelize");
 module.exports = {
   getWords: async (req, res) => {
     const {
-      username,
-      email,
-      password,
-      master
-    } = req.body;
-    if (!username || !email || !password || !master) {
+      length,
+      is_verb,
+      is_noun,
+    } = req.query;
+    if (!length || !is_verb || !is_noun) {
       return res.status(400).json({
-        error: 'You must provide a username, email, password, and master password'
+        error: 'You must provide a length, and choice for both verbs and nouns.'
       });
     }
     try {
-      const user = await Word.create({
-        username,
-        email,
-        password,
-        master,
+      const possibleWords = await Word.findAll({
+        where: {
+          length: {
+            [Op.lte]: length / 2,
+          },
+          is_verb,
+          is_noun,
+        }
       });
-      res.json(user);
+      let words = [];
+      let loopSize = 2;
+      for (let i = 0; i < loopSize; i++) {
+        let randIndex = Math.floor(Math.random() * (possibleWords.length - 1));
+        let newWord = possibleWords[randIndex].word;
+        words.push(newWord);
+      }
+      res.json(words);
     } catch (e) {
       res.json(e);
     }
